5af315d5a94c715c0dec36f456ec173ff83bebb7
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2025-09-21 03:28:58 +0500

    added more products on cookania

--- confectionary-web/src/company-logos/cookania-products/cookania.js
+++ confectionary-web/src/company-logos/cookania-products/cookania.js
@@ -1,11 +1,29 @@
-import KhasKhatai from "../cookania-products/khaskhatai10.png"
-import Zeera from "../cookania-products/zeera10.png"
-import TeatimeSada40 from "../cookania-products/TeatimeSada40.png"
-import TeatimeChoco10 from "../cookania-products/teatimeChoco.png"
+import KhasKhatai from "../cookania-products/khaskhatai10.png";
+import Zeera from "../cookania-products/zeera10.png";
+import TeatimeSada40 from "../cookania-products/TeatimeSada40.png";
+import TeatimeChoco10 from "../cookania-products/teatimeChoco.png";
+import Chocolava from "../cookania-products/chocolava.png";
+import Cocodelight from "../cookania-products/cocodelight.png";
+import DreamCreamChoc from "../cookania-products/dreamCreamChoc.png";
+import DreamCreamStraw from "../cookania-products/dreamCreamStraw.png";
+import Lemon from "../cookania-products/lemon.png";
+import ChocolateSand from "../cookania-products/chocolateSandwich.png";
+import Milkabisk from "../cookania-products/milkabisk.png";
+import Pikanut from "../cookania-products/pikanut.png";
+import Chocochip from "../cookania-products/chocochip.png";
 
 export default {
-    KhasKhatai,
-    Zeera,
-    TeatimeSada40,
-    TeatimeChoco10
+  KhasKhatai,
+  Zeera,
+  TeatimeSada40,
+  TeatimeChoco10,
+  Cocodelight,
+  DreamCreamChoc,
+  DreamCreamStraw,
+  Lemon,
+  ChocolateSand,
+  Milkabisk,
+  Pikanut,
+  Chocolava,
+  Chocochip,
 };
--- confectionary-web/src/company-products/cookania.jsx
+++ confectionary-web/src/company-products/cookania.jsx
@@ -16,10 +16,22 @@ function Cookania() {
         </div>
       </div>
       <div className="flex flex-wrap gap-16 justify-center items-center mx-8">
-        <ProCompo h2="Cookania Khaskhati" src={CookaniaProducts.KhasKhatai} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Milkabisk" src={CookaniaProducts.Milkabisk} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Khaskhati Rs:10" src={CookaniaProducts.KhasKhatai} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Khaskhati Rs:20" src={CookaniaProducts.KhasKhatai} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180}  imgClassName="w-30 h-20 m-14" />
+        <ProCompo h2="Cookania Pikanut" src={CookaniaProducts.Pikanut} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Chocochip" src={CookaniaProducts.Chocochip} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Chocolava" src={CookaniaProducts.Chocolava} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} imgClassName=""/>
+        <ProCompo h2="Cookania Cocodelight" src={CookaniaProducts.Cocodelight} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Lemonsandwich" src={CookaniaProducts.Lemon} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Chocolatesandwich" src={CookaniaProducts.ChocolateSand} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
         <ProCompo h2="Cookania Zeera" src={CookaniaProducts.Zeera} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
-        <ProCompo h2="Cookania Teatime Chocolate" src={CookaniaProducts.TeatimeChoco10} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
-        <ProCompo h2="Cookania Teatime Plain Rs:40" src={CookaniaProducts.TeatimeSada40} perPiece={40} perboxCut={225} perBoxHole={220} perBoxRetail={240} />
+        <ProCompo h2="Cookania Dreamcream StrawBerry" src={CookaniaProducts.DreamCreamStraw} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Dreamcream Chocolate" src={CookaniaProducts.DreamCreamChoc} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Teatime Chocolate Rs:10" src={CookaniaProducts.TeatimeChoco10} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} />
+        <ProCompo h2="Cookania Teatime Chocolate Rs:40" src={CookaniaProducts.TeatimeChoco10} perPiece={40} perboxCut={230} perBoxHole={220} perBoxRetail={240}  imgClassName="w-20 h-30 my-9" />
+        <ProCompo h2="Cookania Teatime Plain Rs:10" src={CookaniaProducts.TeatimeSada40} perPiece={10} perboxCut={170} perBoxHole={160} perBoxRetail={180} imgClassName="w-20 h-50 " />
+        <ProCompo h2="Cookania Teatime Plain Rs:40" src={CookaniaProducts.TeatimeSada40} perPiece={40} perboxCut={225} perBoxHole={220} perBoxRetail={240} imgClassName="m-9" />
       </div>
     </div>
   );
